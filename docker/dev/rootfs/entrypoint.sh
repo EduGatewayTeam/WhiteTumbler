f60408ca49cfd158d71a611c65461ca5e768417f
Nikita Klyshko <n-klyshko@mail.ru>
2020-09-16 16:55:02 +0300

    Initial commit

---
+++ docker/dev/rootfs/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+chown www-data /var/www/storage
+chmod -R a+rw /var/www/storage
+
+NGINX_CONFIG=laravel
+
+# Avoid to remove a bind mounted nginx config
+NGINX_DEFAULT=/etc/nginx/sites-enabled/default
+if ! mountpoint -q $NGINX_DEFAULT; then
+  echo "Using default nginx config : $NGINX_CONFIG"
+  rm $NGINX_DEFAULT
+  ln -s /etc/nginx/sites-available/$NGINX_CONFIG.conf $NGINX_DEFAULT
+fi
+
+# Disable opcache optimisation for developpement
+# Allow files to be reloaded when update without restarting fpm process
+if [[ "$PERFORMANCE_OPTIM" = "false" ]]
+then
+  echo "Disable performance optimisation"
+  echo > /etc/php/7.4/fpm/conf.d/99-laravel.ini
+fi
+
+composer install
+
+exec /usr/bin/supervisord -n
